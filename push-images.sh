e2b16658b1a4028c0986dd1a1828460c258c30bd
Andrew <AndrewDi@users.noreply.github.com>
2019-03-23 21:03:11 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -6,12 +6,9 @@ set -u
 
 for imagepath in $(cat ./imagepath.txt)
 do
-imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
-docker pull $imagepath
-docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
-docker tag $imagepath willdockerhub/$imagename
-# push到阿里云仓库
-docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename && \
-# push到dockerhub
-docker push willdockerhub/$imagename
+  imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
+  docker pull $imagepath
+  docker tag $imagepath registry.cn-shenzhen.aliyuncs.com/kubernetes_sz/$imagename
+  # push到阿里云仓库
+  docker push registry.cn-shenzhen.aliyuncs.com/kubernetes_sz/$imagename 
 done
